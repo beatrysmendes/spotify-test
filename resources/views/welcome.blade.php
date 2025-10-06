e7a221598467b8e0864d49b193f8dd48ed43d21f
nomi <beatrysmendescourageoftheejfod@gmail.com>
2025-10-06 13:10:35 -0300

    qualquer coisa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="
+    https://cdn.jsdelivr.net/npm/reset-css@5.0.2/reset.min.css
+    " rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/styles.css') }}">
     <title>Document</title>
 </head>
 <body>  
-    @for ($i = 0; $i < 10; $i++)
-        @if($i < 5)
-            <span>Contador = {{ $i }} </span>
-        @else
-            <span style="color: red">Contador = {{ $i }} </span>
-        @endif
-    @endfor
+    <header>
+    
+        <img class="logo" src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_White.png">
+
+        <nav>a</nav>
+        <nav>a</nav>
+        <nav>a</nav>
+        <nav>a</nav>
+        <nav>a</nav>
+        
+    </header>
 </body>
 </html>
