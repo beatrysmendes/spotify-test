65af15f2393f125c81cbe54b568a4d0f126c487e
nomi <beatrysmendescourageoftheejfod@gmail.com>
2025-10-08 12:36:22 -0300

    casinha

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,19 +7,32 @@
     https://cdn.jsdelivr.net/npm/reset-css@5.0.2/reset.min.css
     " rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/styles.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>  
     <header>
     
         <img class="logo" src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_White.png">
-
-        <nav>a</nav>
-        <nav>a</nav>
-        <nav>a</nav>
-        <nav>a</nav>
-        <nav>a</nav>
         
+
+        <div class="search-bar"> 
+            <div class="casinha">  
+                <i class="fa-regular fa-house icon-search"></i>
+            </div>
+            <div class="search">
+                <img class="logo icon-search" src="https://images.icon-icons.com/1659/PNG/512/3844432-magnifier-search-zoom_110300.png"> 
+                <input type="text" placeholder="O que queres reproduzir?" class="search-input">
+                <span class="divider"></span>
+                <img class="logo icon-search" src="https://images.icon-icons.com/1673/PNG/512/shoppingbagoutline_110751.png">
+            </div>
+        </div>
+    
+
+        <div>
+            aaaaaaa
+        </div>
+
     </header>
 </body>
 </html>
