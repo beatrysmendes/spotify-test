29f719aac1fbd82a4a721b15b791045e0c0ee934
nomi <beatrysmendescourageoftheejfod@gmail.com>
2025-10-08 13:04:37 -0300

    nav

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,9 +8,10 @@
     " rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="{{ asset('/css/styles.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Document</title>
+    <title>Spotify</title>
 </head>
 <body>  
+    <!-- https://fontawesome.com/search?q=box&o=r -->
     <header>
     
         <img class="logo" src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_White.png">
@@ -18,20 +19,27 @@
 
         <div class="search-bar"> 
             <div class="casinha">  
-                <i class="fa-regular fa-house icon-search"></i>
+                <i class="fa-regular fa-house icon-search icon-search"></i>
             </div>
             <div class="search">
-                <img class="logo icon-search" src="https://images.icon-icons.com/1659/PNG/512/3844432-magnifier-search-zoom_110300.png"> 
+                <i class="fa-solid fa-magnifying-glass icon-search"></i>
                 <input type="text" placeholder="O que queres reproduzir?" class="search-input">
                 <span class="divider"></span>
-                <img class="logo icon-search" src="https://images.icon-icons.com/1673/PNG/512/shoppingbagoutline_110751.png">
+                <i class="fa-solid fa-box-open icon-search"></i>
             </div>
         </div>
-    
-
-        <div>
-            aaaaaaa
-        </div>
+        
+        <nav class="nav">
+            <div class="nav__buttons">
+                <div>botao</div>
+                <div>botao</div>
+            </div>
+            <div class="nav__icons">
+                <div>icone</div>
+                <div>icone</div>
+            </div>
+            <div>avatar</div>
+        </nav>
 
     </header>
 </body>
